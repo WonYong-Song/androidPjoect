b170db81a03832a4aed1ca68c857534b2653190d
whydontusermynameha <beholderstar@gmail.com>
2019-03-22 18:36:16 +0900

    상세보기 작업중

--- app/src/main/java/com/kosmo/studycastle/AcademyDetailView.java
+++ app/src/main/java/com/kosmo/studycastle/AcademyDetailView.java
@@ -1,7 +1,13 @@
 package com.kosmo.studycastle;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.naver.maps.map.MapView;
 
@@ -9,6 +15,13 @@ public class AcademyDetailView extends AppCompatActivity {
 
     //네이버지도 이용을 위한 변수선언
     private MapView mapView;
+
+    //젼역변수
+    ImageView aca_image;
+    TextView category,aca_name,telephone,address,introduce;
+    GridView teacher_intro;
+    ListView class_info;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +31,38 @@ public class AcademyDetailView extends AppCompatActivity {
         //네이버지도 구현 부분
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
+
+        //이전 엑티비이에서 전달한 부가 데이터 읽어오기
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        String idx = bundle.getString("c");
+
+        //AsyncTask를 이용한 서버 접속
+        new AsyncHttpRequest().execute("http://192.168.0.24:8080/FinallyProject/catle/AppAcaDetail.do"
+                ,"idx="+idx
+        );
+    }//onCreate
+
+    class AsyncHttpRequest extends AsyncTask<String,Void,String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
+
+
+
 
     @Override
     protected void onStart() {
